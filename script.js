a8fe9c76582f3f440b028e6430aa535433fc73a0
LilenAmado <65418208+LilenAmado@users.noreply.github.com>
2022-10-10 21:16:29 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -12,3 +12,83 @@ document.addEventListener("keyup", e=>{
   }
 })
 
+// LIST - content:
+
+/*
+ID list: 
+- DW
+- PowerBI
+- SQL
+- GIT
+
+ID content:
+- DW_content
+- PowerBI_content
+- SQL_content
+- GIT_content
+*/
+
+let li_DW = $('#DW');
+let li_PowerBI = $('#PowerBI');
+let li_SQL = $('#SQL');
+let li_GIT = $('#GIT');
+
+let DW_content = $('#DW_content');
+let PowerBI_content = $('#PowerBI_content');
+let SQL_content = $('#SQL_content');
+let GIT_content = $('#GIT_content');
+
+let resumenes = $('#resumenes');
+let bienvenido = $('#bienvenido');
+
+resumenes.hide();
+bienvenido.show();
+
+li_DW.click(()=>{
+
+  resumenes.show();
+  bienvenido.hide();
+
+  PowerBI_content.hide();
+  SQL_content.hide();
+  GIT_content.hide();
+
+  DW_content.show();
+
+});
+
+li_PowerBI.click(()=>{
+  
+  resumenes.show();
+  bienvenido.hide();
+
+  SQL_content.hide();
+  GIT_content.hide();
+  DW_content.hide();
+
+  PowerBI_content.show();
+});
+
+li_SQL.click(()=>{
+  
+  resumenes.show();
+  bienvenido.hide();
+  
+  PowerBI_content.hide();
+  DW_content.hide();
+  GIT_content.hide();
+
+  SQL_content.show();
+});
+
+li_GIT.click(()=>{
+  
+  resumenes.show();
+  bienvenido.hide();
+
+  PowerBI_content.hide();
+  SQL_content.hide();
+  DW_content.hide();
+
+  GIT_content.show();
+});
